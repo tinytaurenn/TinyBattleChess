04c450c4b99e47caab982c9493753647ea032763
tinytaurenn <dumortier.matthieu@gmail.com>
2025-03-19 14:24:28 +0100

    replaced old so assets

--- Assets/SO/Weapons/SO_ShieldWeapon.cs
+++ Assets/SO/Weapons/SO_ShieldWeapon.cs
@@ -1,7 +1,9 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 [CreateAssetMenu(fileName = "SO_Weapon", menuName = "Scriptable Objects/Items/Weapons/SO_Weapon/ShieldWeapon")]
 public class SO_ShieldWeapon : SO_Weapon
 {
-    
+    public List<AudioResource> ParrySounds;
 }
